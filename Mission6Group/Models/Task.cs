b9380d174708c78b970e967032dd19e7964840e8
delangea <73494880+delangea@users.noreply.github.com>
2022-02-07 19:40:57 -0700

    start models

---
+++ Mission6Group/Models/Task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6Group.Models
+{
+    public class Task
+    {
+        [Key]
+        public int TaskId { get; set; }
+        [Required]
+        public string TaskDescription { get; set; }
+        public DateTime DueDate { get; set; }
+        [Required]
+        public int Quadrant { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public bool Completed { get; set; }
+
+
+
+    }
+}
